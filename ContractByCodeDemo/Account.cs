5d60412beade0ba71811e39e6c2207be93bf2584
Morten Laursen <ms.fuge@gmail.com>
2016-12-02 11:56:22 +0100

    init

---
+++ ContractByCodeDemo/Account.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContractByCodeDemo
+{
+    public class Account
+    {
+        private double balance;
+        
+
+        public Account(double balance)
+        {
+            this.balance = balance;
+        } 
+
+        public double Deposit(double amount)
+        {
+            return ChangeBalance(balance + amount);
+        }
+
+        public double Withdraw(double amount)
+        {
+            return ChangeBalance(balance - amount);
+        }
+
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        public double ChangeBalance(double newBalance)
+        {
+            return balance = newBalance;
+        }
+
+
+    }
+}
---
+++ ContractByCodeDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContractByCodeDemo
+{
+    public class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            
+            Program program = new Program();
+        }
+    }
+}
